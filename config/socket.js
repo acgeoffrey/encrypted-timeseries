4a7c0de1a75b266f1f1331001a9987a0ffa6aaac
Geoffrey <acgeoffrey@users.noreply.github.com>
2023-08-29 11:03:10 +0530

    setup: socket

---
+++ config/socket.js
@@ -0,0 +1,1 @@
+module.exports.socketsIo = function (server) {};
--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const http = require('http');
 
 const app = express();
 const port = 8000;
+const httpServer = http.createServer(app);
 
+const io = require('./config/socket').socketsIo(httpServer);
+
+httpServer.listen(3000);
+console.log('sockets is listening.');
+
+// Static files middleware
 app.use(express.static('./assets'));
 
+// Setting up EJS
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.listen(port, () => {
+app.listen(port, (err) => {
+  if (err) {
+    console.log('Error in running the server');
+    return;
+  }
+
   console.log(`Server running on the PORT: ${port}`);
 });
