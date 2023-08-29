9ab649236545bce25b4585995793d32c3b9165da
Geoffrey <acgeoffrey@users.noreply.github.com>
2023-08-29 11:47:09 +0530

    add: minute model

--- models/MinuteModel.js
+++ models/MinuteModel.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose');
 
-const minuteSchema = new mongoose.Schema({
-  minute: {
-    type: String,
-    required: [true, 'Time is required'],
+const minuteSchema = new mongoose.Schema(
+  {
+    minute: {
+      type: String,
+      required: [true, 'Time is required'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+    },
+    messages: [String],
   },
-});
+  {
+    timestamps: true,
+  }
+);
+
+modules.export = mongoose.model('Minute', minuteSchema);
