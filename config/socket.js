cc9dc921b2d991a64f1fc44cfc249b9d12b90875
Geoffrey <acgeoffrey@users.noreply.github.com>
2023-08-30 17:36:58 +0530

    add: socket

--- config/socket.js
+++ config/socket.js
@@ -1,1 +1,7 @@
-module.exports.socketsIo = function (server) {};
+module.exports.socketsIo = function (server) {
+  let io = require('socket.io')(server);
+
+  io.sockets.on('connection', function (socket) {
+    console.log('New Connection Received', socket.id);
+  });
+};
